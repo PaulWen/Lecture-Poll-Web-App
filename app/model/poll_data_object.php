e4afa1eaa6abb0473f90f8650196754b9734e5ec
KimAnhNguyen <kimanhnguyenjcu@yahoo.com>
2016-04-14 23:30:30 +0800

    Comments in file name
    file downloaded is expected to be given in format: ~"ict1.csv"

--- app/model/poll_data_object.php
+++ app/model/poll_data_object.php
@@ -53,10 +53,11 @@ class poll_data_object extends abstract_data_object {
 		return $this->teacherPollCode;
 	}
 	
+	//Name file is supposed to be given in format: "ict1.csv" 
 	public function downloadResult($pollname){
 		// output headers so that the file is downloaded rather than displayed
 		header('Content-Type: text/csv; charset=utf-8');
-		header('Content-Disposition: attachment; filename="$pollname" . "." . "csv"');
+		header('Content-Disposition: attachment; filename= {$pollname}');
 		
 		// create a file pointer connected to the output stream
 		$output = fopen('php://output', 'w');
